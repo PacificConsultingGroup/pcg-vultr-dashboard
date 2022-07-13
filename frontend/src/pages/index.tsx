db300efc3b0bd230f3cf401ce406f21dbeee0a33
CannethPCG <canneth.ho@pacificconsultinggroup.com>
2022-07-13 22:40:08 +0800

    feat: Redirect user to login page if  not logged in

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useLoggedInUser from '@/src/hooks/useLoggedInUser';
 import styles from './index.module.css';
 
 const Home: NextPage = () => {
+
+  const { loggedInUser } = useLoggedInUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loggedInUser) router.replace('/login');
+  }, [loggedInUser, router]);
+
   return (
     <div className={ styles.overallContainer }>
 
