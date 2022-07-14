3e79715a42db9406cf0ebbef0c771bb93f0cb8cf
CannethPCG <canneth.ho@pacificconsultinggroup.com>
2022-07-14 21:31:14 +0800

    feat: Enable transition duration control in usePageTransition

--- frontend/src/hooks/usePageTransition.ts
+++ frontend/src/hooks/usePageTransition.ts
@@ -9,27 +9,29 @@ const defaultOptions: Options = {
   redirectType: 'push'
 };
 
-export default function usePageTransition() {
+export default function usePageTransition(duration = 100) {
+
+  /*
+    PARAMTERS:
+    * duration: The duration of the enter/exit transition. In ms.
+  */
 
   const router = useRouter();
   const bodyNodeRef = useRef<HTMLBodyElement | null>(null);
   const afterPageExitCallbackRef = useRef<(e: TransitionEvent) => void>();
 
   function pageTransitionTo(destinationURL: string, options: Options = defaultOptions) {
-    function afterPageExit(e: TransitionEvent) {
-      if (e.currentTarget !== e.target) return;
-      switch (options.redirectType) {
-        case 'back':
-          router.back();
-          break;
-        case 'replace':
-          router.replace(destinationURL);
-          break;
-        case 'push':
-          router.push(destinationURL);
-          break;
+    const { redirectType } = options;
+    function afterPageExit(e?: TransitionEvent) {
+      if (e?.currentTarget !== e?.target) return;
+      switch (redirectType) {
+        case 'back': return router.back();
+        case 'replace': return router.replace(destinationURL);
+        case 'push': return router.push(destinationURL);
       }
     }
+    if (duration === 0) return afterPageExit();
+    bodyNodeRef.current?.style.setProperty('--page-transition-duration', `${duration}ms`);
     afterPageExitCallbackRef.current = afterPageExit;
     bodyNodeRef.current?.addEventListener('transitionend', afterPageExitCallbackRef.current);
     bodyNodeRef.current?.classList.add('pageFadeOut');
